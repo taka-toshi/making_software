45d341ed15f1ccec8ad87d93aefb3e1d3aff31ec
Nakaguchi Takatoshi <takatoshi.n@fuji.waseda.jp>
2023-05-19 16:43:06 +0900

    1st commit

---
+++ AServer.java
@@ -0,0 +1,208 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.net.*;
+import java.security.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AServer {
+
+    // public static final int PORT = 8080; // ポート番号を設定する．
+
+    public static void main(String[] args) throws IOException {
+        File db = new File("database.db");
+        if (!db.exists()) {
+            try {
+                db.createNewFile();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+        create_table(db);
+
+        final int PORT = 8080; // ポート番号をプログラムの引数で与える
+        ServerSocket s = new ServerSocket(PORT); // ソケットを作成する
+        // System.out.println("Started: " + s);
+        try {
+            Socket socket = s.accept(); // コネクション設定要求を待つ
+            try {
+                // System.out.println("Connection accepted: " + socket);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream())); // データ受信用バッファの設定
+                PrintWriter out = new PrintWriter(
+                        new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
+                        true); // 送信バッファ設定
+
+                out.println("Serverと接続しました。"); // 1
+                Integer option = Integer.parseInt(in.readLine()); // 2
+                String username = in.readLine(); // 3
+                String pass = in.readLine(); // 4
+
+                String hash_pass = make_hash(pass);
+
+                if (option == 1) {
+                    List<String> list_data = new ArrayList<String>();
+                    list_data = login_user(db, username, hash_pass);
+                    Boolean LOGIN = false;
+                    if (list_data.size() == 1) {
+                        LOGIN = true;
+                        out.println(LOGIN);
+                        // while (true) {
+                        // Integer option2 = Integer.parseInt(in.readLine()); // 5
+                        // File chat_log;
+                        // if (option2 == 1) {
+                        // String room_name = in.readLine();
+                        // List<String> list_data2 = new ArrayList<String>();
+                        // list_data2 = check_chat_room(db2, room_name);
+                        // if (list_data2.size() == 1) {
+                        // if (Integer.parseInt(list_data2.get(0)) == 1) {
+                        // out.println(
+                        // "'" + room_name + "'というチャットルームが存在します");
+                        // } else {
+                        // create_chat_room(db2, room_name);
+                        // chat_log = new File(room_name + "_chat_log.txt");
+                        // chat_log.createNewFile();
+                        // out.println("チャットルームを作成しました");
+                        // }
+                        // }
+                        // } else if (option2 == 2) {
+                        // String room_name = in.readLine();
+                        // // config room and join chat room
+                        // List<String> list_data2 = new ArrayList<String>();
+                        // list_data2 = check_chat_room(db2, room_name);
+                        // out.println(room_name + "に参加できました");
+                        // } else if (option2 == 3) {
+                        // // ユーザーが参加しているチャットルームを表示
+                        // List<String> list_data2 = new ArrayList<String>();
+                        // list_data2 = show_chat_room(db2, username);
+                        // out.println(list_data2);
+                        // String room_name = in.readLine();
+                        // chat_log = new File(room_name + "_chat_log.txt");
+                        // // chat log
+                        // BufferedReader chat_log_reader = new BufferedReader(
+                        // new FileReader(chat_log));
+                        // String chat_log_line;
+                        // while ((chat_log_line = chat_log_reader.readLine()) != null) {
+                        // out.println(chat_log_line);
+                        // }
+                        // chat_log_reader.close();
+                        // // chat log
+                        // PrintWriter chat_log_writer = new PrintWriter(
+                        // new BufferedWriter(new FileWriter(chat_log, true)));
+                        // String chat_log_data = in.readLine();
+                        // chat_log_writer.println(chat_log_data);
+                        // chat_log_writer.close();
+                        // } else if (option2 == 4) {
+                        // break;
+                        // }
+                        // }
+                    } else {
+                        out.println(LOGIN);
+                    }
+                } else if (option == 2) {
+                    List<String> list_data2 = new ArrayList<String>();
+                    list_data2 = check_user(db, username);
+                    if (list_data2.size() == 1) {
+                        if (Integer.parseInt(list_data2.get(0)) == 1) {
+                            out.println("'" + username + "'というユーザーが存在します");
+                        } else {
+                            add_user(db, username, hash_pass);
+                            out.println("ユーザーを追加しました");
+                        }
+                    }
+                }
+            } finally {
+                System.out.println("closing...");
+                socket.close();
+            }
+        } finally {
+            s.close();
+        }
+    }
+
+    // execute sql
+    public static void execute_sql(File db, String sql) {
+        String[] cmd = { "sqlite3", db.getAbsolutePath(), sql };
+        try {
+            Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    // execute_sql_return_data
+    public static List<String> execute_sql_return_data(File db, String sql) {
+        List<String> data = new ArrayList<String>();
+        String[] cmd = { "sqlite3", db.getAbsolutePath(), sql };
+        try {
+            Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        return data;
+    }
+
+    // create table
+    public static void create_table(File db) {
+        String sql = "CREATE TABLE IF NOT EXISTS userstable (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT,password TEXT)";
+        execute_sql(db, sql);
+    }
+
+    // add user to database
+    public static void add_user(File db, String username, String password) {
+        String sql = "INSERT INTO userstable(username,password) VALUES ('" +
+                username +
+                "','" +
+                password +
+                "')";
+        execute_sql(db, sql);
+    }
+
+    // login user
+    public static List<String> login_user(
+            File db,
+            String username,
+            String password) {
+        String sql = "SELECT * FROM userstable WHERE username = '" +
+                username +
+                "' AND password = '" +
+                password +
+                "'";
+        List<String> data = new ArrayList<String>();
+        data = execute_sql_return_data(db, sql);
+        return data;
+    }
+
+    // check user
+    public static List<String> check_user(File db, String username) {
+        String sql = "SELECT  EXISTS(SELECT * FROM userstable WHERE username = '" +
+                username +
+                "')AS customer_check;";
+        List<String> data = new ArrayList<String>();
+        data = execute_sql_return_data(db, sql);
+        return data;
+    }
+
+    // sha-256ハッシュ値を返す
+    public static String make_hash(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            return String.format("%064x", new BigInteger(1, md.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Client.java
@@ -0,0 +1,166 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.*;
+
+import java.awt.Rectangle;
+import java.awt.event.*;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        final int PORT = 8080;
+        InetAddress addr = InetAddress.getByName("localhost"); // IP アドレスへの変換
+        // System.out.println("addr = " + addr);
+        // Socket socket = new Socket(addr, JabberServer.PORT); // ソケットの生成
+        Socket socket = new Socket(addr, PORT); // ソケットの生成
+        try {
+            // System.out.println("socket = " + socket);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())); // データ受信用バッファの設定
+            PrintWriter out = new PrintWriter(
+                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
+                    true); // 送信バッファ設定
+            System.out.println(in.readLine()); // 1
+
+            // Create and set up the window.
+            JFrame frame = new JFrame("MyApplication");
+            // frame.setTitle("MyApplication!");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Rectangle table = new Rectangle(1000, 500);
+            frame.setBounds(table);
+            frame.setLocationRelativeTo(null);
+
+            // Add contents to the window.
+            JPanel p = new JPanel();
+            final Integer[] option = { null };
+            // login and signup button with action listener
+            JButton login_btn = new JButton("login");
+            login_btn.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    option[0] = 1;
+                }
+            });
+            JButton signup_btn = new JButton("signup");
+            signup_btn.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    option[0] = 2;
+                }
+            });
+            p.add(login_btn);
+            p.add(signup_btn);
+            frame.getContentPane().add(p);
+            frame.setVisible(true);
+
+            // optionの値が決まるまで以下のコードを実行しない
+            while (true) {
+                if (option[0] == null) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    break;
+                }
+            }
+            out.println(option[0]); // 2
+            frame.getContentPane().removeAll();
+
+            JPanel p2 = new JPanel();
+            JLabel label_user = new JLabel("ユーザー名を入力してください");
+            JLabel label_pass = new JLabel("パスワードを入力してください");
+            JTextField tf_user = new JTextField();
+            JPasswordField tf_pass = new JPasswordField();
+            final Boolean[] ok = { null };
+            JButton ok_btn = new JButton("ok");
+            ok_btn.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    ok[0] = true;
+                }
+            });
+            tf_user.setColumns(10);
+            tf_pass.setColumns(10);
+            p2.add(tf_user);
+            p2.add(tf_pass);
+            p2.add(label_user);
+            p2.add(label_pass);
+            p2.add(ok_btn);
+            frame.add(p2);
+            frame.setVisible(true);
+            frame.validate();
+            frame.repaint();
+
+            String username = tf_user.getText();
+            char[] pass = tf_pass.getPassword();
+            String pass_str = new String(pass);
+
+            while (true) {
+                if (ok[0] == null) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    break;
+                }
+            }
+            out.println(username); // 3
+            out.println(pass_str); // 4
+
+            if (option[0] == 1) {
+                Boolean LOGIN = false;
+                LOGIN = Boolean.parseBoolean(in.readLine());
+                if (LOGIN == true) {
+                    System.out.println("ログインしました!");
+                    // create chat room
+                    // while (true) {
+                    // System.out.println(
+                    // "1.create chat, 2.join chat, 3.start chat, 4.exit");
+                    // Integer option2 = Integer.parseInt(sc.nextLine());
+
+                    // out.println(option2); // 5
+                    // if (option2 == 1) {
+                    // System.out.println("チャットルーム名を入力してください");
+                    // String room_name = sc.nextLine();
+                    // out.println(room_name);
+                    // System.out.println("チャットルームを作成しました");
+                    // // System.out.println("チャットルームを作成しませんでした");
+                    // } else if (option2 == 2) {
+                    // System.out.println(
+                    // "参加するチャットルーム名を入力してください");
+                    // String room_name = sc.nextLine();
+                    // out.println(room_name);
+                    // System.out.println("チャットルームに参加しました");
+                    // // System.out.println("チャットルームに参加しませんでした");
+                    // } else if (option2 == 3) {
+                    // System.out.println("チャットを開始します");
+                    // while (true) {
+                    // String str = in.readLine();
+                    // if (str.equals("END"))
+                    // break;
+                    // System.out.println(str);
+                    // }
+                    // } else if (option2 == 4) {
+                    // System.out.println("終了します");
+                    // break;
+                    // }
+                    // }
+                } else {
+                    System.out.println("ログインできませんできた");
+                }
+            } else if (option[0] == 2) {
+                System.out.println(in.readLine());
+            }
+        } finally {
+            System.out.println("closing...");
+            socket.close();
+            sc.close();
+        }
+    }
+}
