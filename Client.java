f50f6d325a196ceadd2bfee990f0ba4c3ae58060
Nakaguchi Takatoshi <takatoshi.n@fuji.waseda.jp>
2023-05-24 18:08:52 +0900

    fix Client.java

--- Client.java
+++ Client.java
@@ -149,7 +149,7 @@ public class Client {
     // sha-256ハッシュ値を返す
     public static String make_hash(String password) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA256");
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(password.getBytes());
             return String.format("%064x", new BigInteger(1, md.digest()));
         } catch (NoSuchAlgorithmException e) {
