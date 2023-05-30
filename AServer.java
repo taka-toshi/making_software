0aabfadd4abb7642c7156037f15043fba14b2922
Nakaguchi Takatoshi <takatoshi.n@fuji.waseda.jp>
2023-05-30 16:04:45 +0900

    update AServer.java

--- AServer.java
+++ AServer.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AServer extends func {
+    private File db;
+    private int PORT;
+    private ServerSocket s;
 
-    // public static final int PORT = 8080; // ポート番号を設定する．
+    public AServer(String dbPath, int port) throws IOException {
+        db = new File(dbPath);
+        PORT = port;
+        s = new ServerSocket(PORT);
+    }
 
-    public static void main(String[] args) throws IOException {
-        File db = new File("database.db");
+    public void start() throws IOException {
         if (!db.exists()) {
             try {
                 db.createNewFile();
@@ -20,54 +26,59 @@ public class AServer extends func {
         }
         create_table(db);
 
-        final int PORT = 8080; // ポート番号をプログラムの引数で与える
-        ServerSocket s = new ServerSocket(PORT); // ソケットを作成する
-        // System.out.println("Started: " + s);
         try {
             Socket socket = s.accept(); // コネクション設定要求を待つ
-            try {
-                // System.out.println("Connection accepted: " + socket);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream())); // データ受信用バッファの設定
-                PrintWriter out = new PrintWriter(
-                        new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
-                        true); // 送信バッファ設定
-
-                out.println("Serverと接続しました。"); // 1
-                Integer option = Integer.parseInt(in.readLine()); // 2
-                String username = in.readLine(); // 3
-                String pass = in.readLine(); // 4
-
-                String hash_pass = make_hash(pass);
-
-                if (option == 1) {
-                    List<String> list_data = new ArrayList<String>();
-                    list_data = login_user(db, username, hash_pass);
-                    Boolean LOGIN = false;
-                    if (list_data.size() == 1) {
-                        LOGIN = true;
-                        out.println(LOGIN); // 5
-                    } else {
-                        out.println(LOGIN); // 5
-                    }
-                } else if (option == 2) {
-                    List<String> list_data2 = new ArrayList<String>();
-                    list_data2 = check_user(db, username);
-                    if (list_data2.size() == 1) {
-                        if (Integer.parseInt(list_data2.get(0)) == 1) {
-                            out.println("'" + username + "'というユーザーが存在します"); // 6
-                        } else {
-                            add_user(db, username, hash_pass);
-                            out.println("ユーザーを追加しました"); // 6
-                        }
-                    }
-                }
-            } finally {
-                System.out.println("closing...");
-                socket.close();
-            }
+            handleConnection(socket);
         } finally {
             s.close();
         }
+    }
+
+    private void handleConnection(Socket socket) throws IOException {
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())); // データ受信用バッファの設定
+            PrintWriter out = new PrintWriter(
+                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
+                    true); // 送信バッファ設定
+
+            out.println("Serverと接続しました。"); // 1
+            Integer option = Integer.parseInt(in.readLine()); // 2
+            String username = in.readLine(); // 3
+            String pass = in.readLine(); // 4
+
+            String hash_pass = make_hash(pass);
+
+            if (option == 1) {
+                List<String> list_data = new ArrayList<String>();
+                list_data = login_user(db, username, hash_pass);
+                Boolean LOGIN = false;
+                if (list_data.size() == 1) {
+                    LOGIN = true;
+                    out.println(LOGIN); // 5
+                } else {
+                    out.println(LOGIN); // 5
+                }
+            } else if (option == 2) {
+                List<String> list_data2 = new ArrayList<String>();
+                list_data2 = check_user(db, username);
+                if (list_data2.size() == 1) {
+                    if (Integer.parseInt(list_data2.get(0)) == 1) {
+                        out.println("'" + username + "'というユーザーが存在します"); // 6
+                    } else {
+                        add_user(db, username, hash_pass);
+                        out.println("ユーザーを追加しました"); // 6
+                    }
+                }
+            }
+        } finally {
+            System.out.println("closing...");
+            socket.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        AServer server = new AServer("database.db", 8080);
+        server.start();
     }
 }
