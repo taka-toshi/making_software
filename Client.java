ee353f6f0cb279f0619b175eb156c4908b35090d
ReoChiyomaru <reo.chiyomaru@gmail.com>
2023-06-21 05:45:52 +0900

    update 0621 5:45

--- Client.java
+++ Client.java
@@ -75,55 +75,66 @@ public class Client extends func {
                             Boolean JOIN = false;
                             JOIN = Boolean.parseBoolean(in.readLine());// 10.5
 
-                            if (JOIN == true) {
-                                //chatting(frame, join_message, in, out);
-
-                                /*
-                                System.out.println("");
-                                System.out.println("チャットを開始します。");
-                                String room_mem = in.readLine();// 11
-                                System.out.println(room_mem);
-                                System.out.println("あなたは '" + room_mem + "' に参加しています。");
-                                System.out.println("終了する際は'END'で終了してください。");
-                                System.out.println("");
-                                */
-
-                                /*
-                                while (true) {
-                                    System.out.println("メッセージ：");
-                                    String message = sc.nextLine();
-                                    out.println(message);
-
-                                    //String echo = in.readLine();// 13
-                                    //System.out.println("echo:"+ echo);
-
-                                    if (message.equals("END")) {
-                                        System.out.println("");
-                                        break;
-                                    }
-                                }
-                                */
+                            if (JOIN == true) {// チャットルームに参加できたとき
 
                                 while (true) {
+                                     
                                     frame.getContentPane().removeAll();// パネルを取り除く
-                                    //System.out.println(in.readLine());// 13
 
                                     Boolean chat_option []= { null };
                                     Boolean quit_option []= { null };
-                                    List<Boolean[]> option3 = new ArrayList<Boolean[]>();
-                                    option3 = List.of(chat_option, quit_option);
-                                    option3 = main_chat_panel(frame, out, join_message, option3); // チャットルームの参加のパネルを表示 p8
-                                    chat_option = option3.get(0);
-                                    quit_option = option3.get(1);
-                                    if(quit_option != null && quit_option[0] == true){
-                                        break;
-                                    }
 
+                                    JPanel p8 = new JPanel();
+                                    p8.setLayout(null);
+
+                                    JLabel label_chat_success = new JLabel(join_message);
+                                    // chat_log.txtを表示
+                                    JLabel label_messagelabel = new JLabel("メッセージ：");
+                                    JTextField tf_message = new JTextField();
+                                    JButton send_btn = new JButton("SEND");
+                                    JButton quit_btn = new JButton("QUIT");
+
+                                    send_btn.addActionListener(new ActionListener() {
+                                        public void actionPerformed(ActionEvent e) {
+                                            String message = tf_message.getText();
+                                            out.println(message);// 12
+
+                                            chat_option[0] = true;
+
+                                        }
+                                    });
+
+                                    quit_btn.addActionListener(new ActionListener() {
+                                        public void actionPerformed(ActionEvent e) {
+                                            out.println("END");// 12
+
+                                            quit_option[0] = true;
+                                        }
+                                    });
+
+                                    tf_message.setColumns(10);
+                                    label_chat_success.setBounds(400, 10, 800, 25);
+                                    label_messagelabel.setBounds(300, 350, 200,25);
+                                    label_messagelabel.setHorizontalAlignment(JLabel.RIGHT);
+                                    tf_message.setBounds(500, 350, 200,25);
+                                    send_btn.setBounds(700, 350, 60,25);
+                                    quit_btn.setBounds(480, 400, 35,25);
+                                    p8.add(label_chat_success);
+                                    p8.add(label_messagelabel);
+                                    p8.add(tf_message);
+                                    p8.add(send_btn);
+                                    p8.add(quit_btn);
+                                    frame.add(p8);
+                                    frame.setVisible(true);
+                                    frame.validate();
+                                    frame.repaint();// 画面を書き直す
+                                    
                                     // optionの値が決まるまで以下のコードを実行しない
                                     while (true) {
-                                        if (chat_option != null && chat_option[0] == true) {
-                                        //chatting(frame, join_message, in, out); // this is important!!
-                                        break;
+                                        if (chat_option [0] != null) {
+                                            break;
+                                        }else if (quit_option [0] != null){
+                                            break;
                                         }else{
                                             try {
                                                 Thread.sleep(100);
@@ -132,9 +143,12 @@ public class Client extends func {
                                             }
                                         }
                                     }
-                                }
 
-                            } else {
+                                    if(quit_option[0] != null){
+                                        break;
+                                    }
+                                }
+                            } else {// チャットルームに参加できなかったとき
                                 frame.getContentPane().removeAll();// パネルを取り除く
                                 fail_join_panel(frame, join_message); // チャットルームに参加できなかったパネルを表示 p9
                             }
@@ -456,10 +470,11 @@ public class Client extends func {
             }
         }
     }
+    /* 
     // chat_optionとquit_optionを返す
     private static List<Boolean[]> main_chat_panel(JFrame frame, PrintWriter out, String join_message, List<Boolean[]> option3) throws IOException {
-        Boolean[] chat_option = option3.get(0);
-        Boolean[] quit_option = option3.get(1);
+        //Boolean[] chat_option = option3.get(0);
+        //Boolean[] quit_option = option3.get(1);
         // パネルp8の実装
         JPanel p8 = new JPanel();
         p8.setLayout(null);
@@ -475,9 +490,10 @@ public class Client extends func {
             public void actionPerformed(ActionEvent e) {
                 String message = tf_message.getText();
                 out.println(message);// 12
-                System.out.println(message);
+                //System.out.println(message);
 
                 chat_option[0] = true;
+
             }
         });
 
@@ -505,9 +521,12 @@ public class Client extends func {
         frame.setVisible(true);
         frame.validate();
         frame.repaint();// 画面を書き直す
-        return Arrays.asList(chat_option, quit_option);
-    }
 
+        //System.out.println(quit_option[0]);
+        //System.out.println(chat_option[0]);
+        //return Arrays.asList(chat_option, quit_option);
+    }
+    */
     private static void fail_join_panel(JFrame frame, String join_message) throws IOException {
         // パネルp9の実装
         JPanel p9 = new JPanel();
@@ -621,7 +640,7 @@ public class Client extends func {
             }
         }
     }
-
+    
     private static void succuss_signup_panel(JFrame frame, BufferedReader in, PrintWriter out, Scanner sc) throws IOException {
         // パネルp11の実装
         JPanel p11 = new JPanel();
@@ -699,17 +718,17 @@ public class Client extends func {
         }
     }
 
-    /*
-    public static void chatting(JFrame frame, String join_message, BufferedReader in, PrintWriter out) {
+    /* 
+    private static void chatting(JFrame frame, String join_message, BufferedReader in, PrintWriter out) {
         frame.getContentPane().removeAll();//パネルp7を取り除く
 
         //パネルp8の実装
         JPanel p8 = new JPanel();
         p8.setLayout(null);
-
+         
         Boolean chat_option []= { null };
         Boolean quit_option []= { null };
-
+        
         JLabel label_chat_success = new JLabel(join_message);
         //chat_log.txtを表示
         JLabel label_messagelabel = new JLabel("メッセージ：");
@@ -751,7 +770,7 @@ public class Client extends func {
         frame.setVisible(true);
         frame.validate();
         frame.repaint();//画面を書き直す
-
+        
         // optionの値が決まるまで以下のコードを実行しない
         while (true) {
             if (chat_option[0] == true){
@@ -766,6 +785,8 @@ public class Client extends func {
                 }
             }
         }
+        
     }
     */
+    
 }
