924bf60faff3d098de1528b41a653fd83a5ffa4f
ReoChiyomaru <reo.chiyomaru@gmail.com>
2023-06-20 21:40:16 +0900

    update 0620 21:39

--- func.java
+++ func.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.math.BigInteger;
 import java.security.*;
 
+
 public class func extends Thread {
     // execute sql
     public static void execute_sql(File db, String sql) {
